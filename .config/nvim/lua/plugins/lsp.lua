89e53f9a6d562c111049b4d2dc45fa1e6731483e
Euihyun Lee <lee@euihyun.dev>
2024-08-07 10:29:26 +0900

    Ensure clangd language server is installed

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -34,6 +34,7 @@ return {
 
             require('mason-lspconfig').setup({
                 ensure_installed = {
+                    'clangd',
                     'lua_ls',
                 },
                 handlers = {
