dcefbed5c9371a3dc0b95ab80f94e8af586c5548
Euihyun Lee <lee@euihyun.dev>
2024-08-11 20:43:35 +0900

    Edit Telescope keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -19,10 +19,16 @@ return {
         require('telescope').load_extension('fzf')
 
         local builtin = require 'telescope.builtin'
+
+        -- File Pickers
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
-        --vim.keymap.set('n', '<leader>fg', builtin.gid_files, { desc = 'Find Git files' })
-        vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Find live grep' })
+        vim.keymap.set('n', '<leader>fF', builtin.git_files, { desc = 'Find Git files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Find grep' })
+
+        -- Vim Pickers
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
+        vim.keymap.set('n', '<leader>fq', builtin.quickfix, { desc = 'Find quickfix' })
+        vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = 'Find in jumplist' })
     end,
 }
